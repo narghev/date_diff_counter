ecb607e7af1a5c9ea6ee8d9077166a80347faf57
narghev <n.ghevandiani@gmail.com>
2018-09-25 22:52:18 +0400

    refactor & add start_date

--- src/application.js
+++ src/application.js
@@ -2,44 +2,68 @@ import React from 'react';
 import Date from 'components/date';
 import moment from 'moment';
 import {
-  getEndDate,
-  setEndDate,
-  clearEndDate
+  getDate,
+  setDate,
+  clearDate
 } from 'helper/date_helper';
+
+const end = 'end';
+const start = 'start';
 
 export default
 class App extends React.Component {
 
   state = {
+    startDate: null,
     endDate: null
   };
 
   componentDidMount(){
-    const endDate = getEndDate();
-    this.setState({endDate});
+    const endDate = getDate(end);
+    const startDate = getDate(start);
+    this.setState({endDate, startDate});
   }
 
   onEndDateChange = date => {
     const endDate = moment(date);
-    setEndDate(endDate);
+    setDate(end, endDate);
     this.setState({endDate});
   }
 
   clearEndDate = () => {
-    clearEndDate();
+    clearDate(end);
     this.setState({endDate: null});
   };
 
+  onStartDateChange = date => {
+    const startDate = moment(date);
+    setDate(start, startDate);
+    this.setState({startDate});
+  }
+
+  clearStartDate = () => {
+    clearDate(start);
+    this.setState({startDate: null});
+  };
+
   render() {
-    const {endDate} = this.state;
+    const {endDate, startDate} = this.state;
 
     return (
-      <Date
-        label="End Date"
-        date={endDate}
-        onChange={this.onEndDateChange}
-        onClear={this.clearEndDate}
-      />
+      <div className="start_end_wrapper">
+        <Date
+          label="Start Date"
+          date={startDate}
+          onChange={this.onStartDateChange}
+          onClear={this.clearStartDate}
+        />
+        <Date
+          label="End Date"
+          date={endDate}
+          onChange={this.onEndDateChange}
+          onClear={this.clearEndDate}
+        />
+      </div>
     );
   }
 }
--- src/helper/date_helper.js
+++ src/helper/date_helper.js
@@ -1,7 +1,7 @@
-export const setEndDate = date => {
-  localStorage.setItem('end_date', date);
+export const setDate = (whichDate, date) => {
+  localStorage.setItem(`${whichDate}_date`, date);
 };
 
-export const getEndDate = () => localStorage.getItem('end_date') || null;
+export const getDate = whichDate => localStorage.getItem(`${whichDate}_date`) || null;
 
-export const clearEndDate = () => localStorage.removeItem('end_date');
+export const clearDate = whichDate => localStorage.removeItem(`${whichDate}_date`);
